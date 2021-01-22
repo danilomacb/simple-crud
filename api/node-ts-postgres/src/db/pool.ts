7f195e76afefc706baffe5470252a2ff344c264c
danilomacb <danilo.bakun@gmail.com>
2021-01-22 07:34:18 -0300

    node-ts-postgres created

---
+++ api/node-ts-postgres/src/db/pool.ts
@@ -0,0 +1,5 @@
+import { Pool } from "pg";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+export default new Pool({ connectionString });
---
+++ api/node-ts-postgres/src/index.ts
@@ -0,0 +1,67 @@
+require("dotenv").config();
+
+import express from "express";
+
+import pool from "./db/pool";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  const { content } = req.body;
+
+  try {
+    await pool.query("INSERT INTO elements (content) VALUES ($1)", [content]);
+  } catch (err) {
+    console.log("Error on create element\n", err);
+    res.status(500).send("Error on create element");
+    return;
+  }
+
+  res.status(201).send("Element created");
+});
+
+app.get("/", async (req, res) => {
+  let elements;
+  try {
+    elements = await pool.query("SELECT * FROM elements ORDER BY ID ASC");
+  } catch (err) {
+    console.log("Error on list all elements\n", err);
+    res.status(500).send("Error on list all elements");
+    return;
+  }
+
+  res.status(200).json(elements.rows);
+});
+
+app.put("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { content } = req.body;
+
+  try {
+    await pool.query("UPDATE elements SET content = $1 WHERE id = $2", [content, id]);
+  } catch (err) {
+    console.log("Error on update element\n", err);
+    res.status(500).send("Error on update element");
+    return;
+  }
+
+  res.status(200).send("Element updated");
+});
+
+app.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  try {
+    await pool.query("DELETE FROM elements WHERE id = $1", [id]);
+  } catch (err) {
+    console.log("Error on delete element\n", err);
+    res.status(500).send("Error on delete element");
+    return;
+  }
+
+  res.status(200).send("Element deleted");
+});
+
+app.listen(3001, () => console.log(`Api running`));
